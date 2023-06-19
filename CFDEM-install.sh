06ea3d79daeb87cfd357d1ad9c3a4227ba61341c
Edward Yang <edward_yang_125@hotmail.com>
2023-06-19 09:24:26 +0000

    Move cfdem user envs to dockerfile

--- CFDEM-install.sh
+++ CFDEM-install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # CFDEM-install.sh
 
-sed -i 's@#export CFDEM_VERSION=PUBLIC@export CFDEM_VERSION=PUBLIC@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_PROJECT_DIR=$HOME/CFDEM/CFDEMcoupling-$CFDEM_VERSION-$WM_PROJECT_VERSION@export CFDEM_PROJECT_DIR=/usr/local/CFDEM/CFDEMcoupling-$CFDEM_VERSION@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_PROJECT_USER_DIR=$HOME/CFDEM/$LOGNAME-$CFDEM_VERSION-$WM_PROJECT_VERSION@export CFDEM_PROJECT_USER_DIR=/home/CFDEM/CFDEMcoupling-$CFDEM_VERSION-logs@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_bashrc=$CFDEM_PROJECT_DIR/src/lagrangian/cfdemParticle/etc/bashrc@export CFDEM_bashrc=$CFDEM_PROJECT_DIR/src/lagrangian/cfdemParticle/etc/bashrc@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_LIGGGHTS_SRC_DIR=$HOME/LIGGGHTS/LIGGGHTS-PUBLIC/src@export CFDEM_LIGGGHTS_SRC_DIR=/usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_LIGGGHTS_MAKEFILE_NAME=auto@export CFDEM_LIGGGHTS_MAKEFILE_NAME=mpi@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@#export CFDEM_LPP_DIR=$HOME/LIGGGHTS/mylpp/src@export CFDEM_LPP_DIR=/usr/local/LIGGGHTS/lpp/src@' src/lagrangian/cfdemParticle/etc/bashrc
-sed -i 's@-6.2@-6.3@g' /usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src/MAKE/Makefile.mpi
-sed -i 's@#-L/usr/lib/x86_64-linux-gnu@-L/usr/lib/x86_64-linux-gnu@' /usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src/MAKE/Makefile.mpi
+cfdembashrc=src/lagrangian/cfdemParticle/etc/bashrc
+liggghtsmakefile=/usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src/MAKE/Makefile.mpi
+sed -i 's@-6.2@-6.3@g' $liggghtsmakefile
+sed -i 's@#-L/usr/lib/x86_64-linux-gnu@-L/usr/lib/x86_64-linux-gnu@' $liggghtsmakefile
+sed -i 's@make new dirs $CFDEM_PROJECT_USER_DIR ? (y/n)@not making new dirs $CFDEM_PROJECT_USER_DIR@' $cfdembashrc
+sed -i 's@read YN@YN=n@' $cfdembashrc
 . /usr/local/OpenFOAM/OpenFOAM-5.x/etc/bashrc
 . src/lagrangian/cfdemParticle/etc/bashrc
 export WM_NCOMPPROCS=`grep -c ^processor /proc/cpuinfo`
--- Dockerfile
+++ Dockerfile
@@ -21,6 +21,13 @@ RUN bash OF-install.sh
 WORKDIR /usr/local/LIGGGHTS
 RUN git clone https://github.com/CFDEMproject/LIGGGHTS-PUBLIC.git && git clone https://github.com/CFDEMproject/LPP.git lpp
 
+ENV CFDEM_VERSION=PUBLIC
+ENV CFDEM_PROJECT_DIR=/usr/local/CFDEM/CFDEMcoupling-PUBLIC
+ENV CFDEM_PROJECT_USER_DIR=/home/CFDEM/cfdemlogs
+ENV CFDEM_BASHRC=/usr/local/CFDEM/CFDEMcoupling-PUBLIC/src/lagrangian/cfdemParticle/etc/bashrc
+ENV CFDEM_LIGGGHTS_SRC_DIR=/usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src
+ENV CFDEM_LIGGGHTS_MAKEFILE_NAME=mpi
+ENV CFDEM_LPP_DIR=/usr/local/LIGGGHTS/lpp/src
 WORKDIR /usr/local/CFDEM
 RUN git clone https://github.com/CFDEMproject/CFDEMcoupling-PUBLIC.git
 WORKDIR /usr/local/CFDEM/CFDEMcoupling-PUBLIC
@@ -34,4 +41,4 @@ RUN useradd docker-user
 RUN mkdir /work && chown docker-user:docker-user /work
 USER docker-user
 WORKDIR /work
-ENTRYPOINT ["/etc/entrypoint.sh"]
+ENTRYPOINT /bin/bash /etc/entrypoint.sh
