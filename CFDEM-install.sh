0fa16b68bb0a5e85f0a2fab20d51a0b93e226c00
Edward Yang <edward_yang_125@hotmail.com>
2023-06-23 10:13:49 +1000

    Install VTK from source

--- CFDEM-install.sh
+++ CFDEM-install.sh
@@ -2,9 +2,8 @@
 # CFDEM-install.sh
 
 cfdembashrc=src/lagrangian/cfdemParticle/etc/bashrc
-liggghtsmakefile=/usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src/MAKE/Makefile.mpi
-sed -i 's@-6.2@-6.3@g' $liggghtsmakefile
-sed -i 's@#-L/usr/lib/x86_64-linux-gnu@-L/usr/lib/x86_64-linux-gnu@' $liggghtsmakefile
+liggghtsmakefile=/usr/local/LIGGGHTS/LIGGGHTS-PUBLIC/src/MAKE/Makefile.auto
+sed -i 's@AUTOINSTALL_VTK ?= "OFF"@AUTOINSTALL_VTK ?= "ON"@' $liggghtsmakefile
 sed -i 's@read YN@YN=y@' $cfdembashrc
 . /usr/local/OpenFOAM/OpenFOAM-5.x/etc/bashrc
 . src/lagrangian/cfdemParticle/etc/bashrc
